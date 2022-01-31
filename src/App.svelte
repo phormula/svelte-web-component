b9bda31f35054d750fbfb6cbb61cc605e03de72b
Evans Teiko Tetteh <evans.teiko@loanxchain.com>
2022-01-31 09:51:21 +0100

    initail commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import "./web-component/main";
+  import "./web-component/polyfills";
+  import "./web-component/runtime";
+</script>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+  />
+</svelte:head>
+<main>
+  <ui-searchtable />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
